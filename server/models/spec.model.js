24c8b73cf68418c25e0da015719be4c7bf80df35
Michael Goncharov <michgonch@gmail.com>
2020-08-23 20:35:10 +0300

    Change Spec model "_id" default field to "id"

--- server/models/spec.model.js
+++ server/models/spec.model.js
@@ -1,17 +1,20 @@
 const specModel = mongoose => {
-  const Spec = mongoose.model(
-    "spec",
-    mongoose.Schema(
-      {
-        title: String,
-        description: String,
-        started: Boolean
-      },
-      { timestamps: true }
-    )
+  const schema = mongoose.Schema(
+    {
+      title: String,
+      description: String,
+      started: Boolean
+    },
+    { timestamps: true }
   );
 
-  return Spec;
+  schema.method("toJSON", function () {
+    const { __v, _id, ...object } = this.toObject();
+    object.id = _id;
+    return object;
+  });
+
+  return mongoose.model("spec", schema);
 };
 
 module.exports = specModel;
